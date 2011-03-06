c74462b4532a811ed8b9cd4dd1c6445e10f06d96
Dylan Richard <github@detour1999.com>
2011-03-06 14:07:48 -0600

    seriously, about the worst there is at underscores.

--- socialregistration/views.py
+++ socialregistration/views.py
@@ -113,8 +113,8 @@ def setup(request, template='socialregistration/setup.html', form_class=UserForm
             user.email = profile.email
             user.save()
 	if profile.real_name:
-	    user.firstname = profile.real_name.partition(' ')[0]
-	    user.lastname = profile.real_name.partition(' ')[2]
+	    user.first_name = profile.real_name.partition(' ')[0]
+	    user.last_name = profile.real_name.partition(' ')[2]
 	    user.save()
 
         # Authenticate and login
